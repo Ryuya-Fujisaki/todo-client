4940f8d9ff534636aa1c7d320cd0b198f3a323ea
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2023-11-29 10:33:46 +0800

    imported react hook etc

---
+++ server.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+global.Headers = fetch.Headers;
+
+// Import and configure the rest of your Next.js server
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
